f456bcf7b6b0ed8ee1c2403e6ac27eb8493a6341
utamuratov <43562558+utamuratov@users.noreply.github.com>
2023-04-24 20:03:32 +0500

    Library worked

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'intersection';
+
+  handleIntersection(sectionNumber: number, threshold = 75) {
+    alert(
+      `Section: ${sectionNumber}. Intersected ${threshold} percentage of the observed element`
+    );
+  }
 }
