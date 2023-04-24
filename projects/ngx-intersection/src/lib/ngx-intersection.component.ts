1124d75a16351987b76720568e0af07e75cce971
utamuratov <43562558+utamuratov@users.noreply.github.com>
2023-04-24 17:19:22 +0500

    Intersection logic added

--- projects/ngx-intersection/src/lib/ngx-intersection.component.ts
+++ projects/ngx-intersection/src/lib/ngx-intersection.component.ts
@@ -1,15 +1,71 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnDestroy,
+  Output,
+} from '@angular/core';
 
 @Component({
-  selector: 'ngx-ngx-intersection',
-  template: `
-    <p>
-      ngx-intersection works!
-    </p>
-  `,
-  styles: [
-  ]
+  selector: 'ngx-intersection',
+  standalone: true,
+  template: ` <ng-content></ng-content> `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class NgxIntersectionComponent {
+export class NgxIntersectionComponent implements AfterViewInit, OnDestroy {
+  /**
+   *
+   * threshold is the percentage of the observed element within the root.
+   * Use this to trigger an intersection when a percentage of the observed
+   * element is within the root i.e. trigger when 65% of my element
+   * is within the viewport (root). Values are % based
+   * between 0 and 1. i.e. threshold: .65
+   *
+   */
+  @Input()
+  threshold = 0.75;
 
+  /**
+   *
+   */
+  @Output()
+  isIntersecting = new EventEmitter<boolean>();
+
+  /**
+   *
+   */
+  private observer!: IntersectionObserver;
+
+  /**
+   *
+   */
+  constructor(private el: ElementRef) {}
+
+  /**
+   *
+   */
+  ngAfterViewInit() {
+    this.observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting === true) {
+          this.isIntersecting.emit(true);
+        }
+      },
+      {
+        threshold: this.threshold,
+      }
+    );
+
+    this.observer.observe(this.el.nativeElement as HTMLElement);
+  }
+
+  /**
+   *
+   */
+  ngOnDestroy() {
+    this.observer.disconnect();
+  }
 }
--- projects/ngx-intersection/src/public-api.ts
+++ projects/ngx-intersection/src/public-api.ts
@@ -2,6 +2,4 @@
  * Public API Surface of ngx-intersection
  */
 
-export * from './lib/ngx-intersection.service';
 export * from './lib/ngx-intersection.component';
-export * from './lib/ngx-intersection.module';
